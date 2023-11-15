d704e0dbb77fa62f3d860aec0b029ce268bfc6ba
20120529 <huynhluat.dn@gmail.com>
2023-11-15 08:36:25 +0700

    add cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 
 const { PORT } = require("./src/config/constant");
+var cors = require("cors");
 
 const app = express();
 const bodyParser = require("body-parser");
@@ -9,6 +10,7 @@ const router = require("./src/router/index");
 const errorHandlingMDW = require("./src/middleware/error-handling");
 const connectDB = require("./src/config/db-connect");
 
+app.use(cors);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
